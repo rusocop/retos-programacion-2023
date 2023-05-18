f90ee545f01272f558a94a1aa4e2952d14dc8d4c
Synok / paellarusa99 <48700894+rusocop@users.noreply.github.com>
2023-05-18 15:23:39 +0200

    paellarusa99.cs
    Reto #20 - C#

---
+++ Retos/Reto #20 - LA TRIFUERZA [Media]/c#/paellarusa99.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public class Trifuerza
+{
+    public static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        int fila;
+        int espacios = 2 * num - 1;
+        int filas = 2 * num + 1; // ("+1" porque el for empieza en el 1 en vez de 0)
+
+        for (fila = 1; fila < (filas); fila++)
+        {
+            for (int n = 0; n < espacios; n++)
+            {
+                Console.Write(" ");
+            }
+
+            espacios--;
+
+            if (fila % 2 == 1) // impar
+            {
+                for (int impar = 0; impar < ((fila / 2) + 1); impar++)
+                {
+                    Console.Write("*   ");
+                }
+                Console.WriteLine();
+            }
+
+            else // par
+            {
+                for (int par = 0; par < (fila / 2); par++)
+                {
+                    Console.Write("*** ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
